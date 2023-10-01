d1afa4150a17ccf0db7beddb304cdf93131b5ed7
oomsinsukit <oomsinsukit@gmail.com>
2023-10-01 17:17:17 +0700

    ----

--- NEW/Python_MLModel.py
+++ NEW/Python_MLModel.py
@@ -16,6 +16,9 @@ from sklearn.model_selection import train_test_split, cross_validate
 from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error, mean_squared_error, r2_score
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 
 def RF(x_train,y_train):
     model = RandomForestRegressor()
@@ -23,13 +26,13 @@ def RF(x_train,y_train):
     model.fit(x_train,y_train)
     return model
 
-def Ridge(x_train,y_train):
+def Ridge_M(x_train,y_train):
     model = Ridge()
     model_cv = cross_validate(model, x_train, y_train, cv=5, return_train_score=True)
     model.fit(x_train,y_train)
     return model
 
-def SVR(x_train,y_train):
+def SVC_R(x_train,y_train):
     model = SVR()
     model_cv = cross_validate(model, x_train, y_train, cv=5, return_train_score=True)
     model.fit(x_train,y_train)
@@ -39,4 +42,13 @@ def XGB(x_train,y_train):
     model = GradientBoostingRegressor()
     model_cv = cross_validate(model, x_train, y_train, cv=5, return_train_score=True)
     model.fit(x_train,y_train)
+    return model
+
+def NN(x_train,y_train):
+    model = Sequential()
+    model.add(Dense(1024, input_dim=x_train.shape[1], activation='relu'))
+    model.add(Dense(512, activation='relu'))
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    model.fit(x_train, y_train, epochs=100, batch_size=16)
     return model
