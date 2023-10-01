324593c629dcbfeeb5c2bf98c99fd9e37e78a8a2
Sawahiko <93868260+Sawahiko@users.noreply.github.com>
2023-10-01 11:53:38 +0700

    shorten fingerpirnt

--- Compare ML_model MF_bit datapoint/MLTESTFILE.py
+++ Compare ML_model MF_bit datapoint/MLTESTFILE.py
@@ -33,28 +33,31 @@ from rdkit.ML.Descriptors import MoleculeDescriptors
 # My module
 from Python_Scoring_Export import Scoring, Export
 from MLModel import RF
-# %% Setup
+
+# %% Option
 MF_bit = 2**10
+MF_radius = 4
 
 # %% Import Data : 560 datapoint
-#Import Data
+# Import Data
 df = pd.read_excel("../DataTb.xlsx",sheet_name="AllDataSet")
 
-#Select feature for data: X=SMILE, Y=Tb
+# Select feature for data: X=SMILE, Y=Tb
 X_data_excel= df[["SMILES"]]
 Y_data= df["Tb"]
 
-#>>> SHOW X_Data, Y_data
-
 # %% Data Preparation
-#Generate Fingerprint from SMILE
+# Generate Fingerprint from SMILE
 X_data_use = X_data_excel.copy()
 X_data_use["molecule"] = X_data_use["SMILES"].apply(lambda x: Chem.MolFromSmiles(x))
-X_data_use["morgan_fp"] = X_data_use["molecule"].apply(lambda x: rdMolDescriptors.GetMorganFingerprintAsBitVect(x, radius=4, nBits=MF_bit, useFeatures=True, useChirality=True))
 
-#>>> SHOW X_data_use 
+X_data_use["morgan_fp"] = X_data_use["molecule"].apply(lambda x: rdMolDescriptors.GetMorganFingerprintAsBitVect(
+        x, 
+        radius=MF_radius, 
+        nBits=MF_bit, 
+        useFeatures=True, useChirality=True))
 
-#Transfrom Fingerprint to Column in DataFrame
+# Transfrom Fingerprint to Column in DataFrame
 X_data_fp = []
 for i in range(X_data_use.shape[0]):
     array = np.array(X_data_use["morgan_fp"][i])
@@ -62,30 +65,21 @@ for i in range(X_data_use.shape[0]):
     datafram_i = datafram_i.T
     X_data_fp.append(datafram_i)
 X_data_fp = pd.concat(X_data_fp, ignore_index=True)
-X_data_ML = pd.concat([X_data_use, X_data_fp], axis=1, join='inner')
 
-Y_data_ML = Y_data.copy()
-
-#>>>  SHOW X_data_fp, Y_data_fp
-
-# %% Train test split
-
-X_train_ML, X_test_ML, y_train_ML, y_test_ML = train_test_split(X_data_ML, Y_data_ML,test_size=0.25,random_state=42)
-
-X_train = X_train_ML.copy().drop(columns = {"SMILES", "molecule", "morgan_fp"})
-X_test = X_test_ML.copy().drop(columns = {"SMILES", "molecule", "morgan_fp"})
-x_total = X_data_ML.copy().drop(columns = {"SMILES", "molecule", "morgan_fp"})
-
-y_train = y_train_ML.copy()
-y_test = y_test_ML.copy()
-y_total = Y_data_ML.copy()
-
-# >>> EXAMPLE SHOW {X, Y} of {TRAIN TEST TOTAL}
+Y_data_fp = Y_data.copy()
 
 # %%
-# Modeling
+# Train-test_Modeling & Cross Validation Modeling
+X_train_fp, X_test_fp, y_train_fp, y_test_fp = train_test_split(X_data_fp, Y_data_fp,
+                                                                test_size=0.25,
+                                                                random_state=42)
+RF_model = RF(X_train_fp, y_train_fp)
 
-RF_model = RF(X_train, y_train)
-
-Score_table = Scoring(RF_model, X_train, X_test, x_total, y_train, y_test, y_total)
+# %%
+# Scoring & Export
+Score_table = Scoring(RF_model, X_train_fp, X_test_fp, X_data_fp, y_train_fp, y_test_fp, Y_data_fp)
 Export(Score_table, "TESTFILE.csv")
+
+# %%
+# Delete variable
+del i, array, datafram_i
