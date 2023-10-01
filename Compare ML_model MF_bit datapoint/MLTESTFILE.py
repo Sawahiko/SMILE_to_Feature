e78c0e8359290a46ecc30bf7a8b1a4eac2d7f1ef
Sawahiko <93868260+Sawahiko@users.noreply.github.com>
2023-10-01 10:53:17 +0700

    Scoring Export

---
+++ Compare ML_model MF_bit datapoint/MLTESTFILE.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 18 09:09:55 2023
+
+@author: oomsin
+"""
+
+# Python
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import seaborn as sn
+import time
+
+
+# Machine Learning
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_percentage_error, mean_squared_error, r2_score
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+
+# RDKit
+from rdkit.Chem import Descriptors
+from rdkit.ML.Descriptors import MoleculeDescriptors
+from rdkit.Chem import rdMolDescriptors
+from rdkit.Chem import AllChem
+from rdkit import Chem
+from rdkit.Chem import Descriptors
+from rdkit.ML.Descriptors import MoleculeDescriptors
+
+# My module
+from Python_Scoring_Export import Scoring, Export
+
+# %% Setup
+MF_bit = 2**10
+
+# %% Import Data : 560 datapoint
+#Import Data
+df = pd.read_excel("../DataTb.xlsx",sheet_name="AllDataSet")
+
+#Select feature for data: X=SMILE, Y=Tb
+X_data_excel= df[["SMILES"]]
+Y_data= df["Tb"]
+
+#>>> SHOW X_Data, Y_data
+
+# %% Data Preparation
+#Generate Fingerprint from SMILE
+X_data_use = X_data_excel.copy()
+X_data_use["molecule"] = X_data_use["SMILES"].apply(lambda x: Chem.MolFromSmiles(x))
+X_data_use["morgan_fp"] = X_data_use["molecule"].apply(lambda x: rdMolDescriptors.GetMorganFingerprintAsBitVect(x, radius=4, nBits=MF_bit, useFeatures=True, useChirality=True))
+
+#>>> SHOW X_data_use 
+
+#Transfrom Fingerprint to Column in DataFrame
+X_data_fp = []
+for i in range(X_data_use.shape[0]):
+    array = np.array(X_data_use["morgan_fp"][i])
+    datafram_i = pd.DataFrame(array)
+    datafram_i = datafram_i.T
+    X_data_fp.append(datafram_i)
+X_data_fp = pd.concat(X_data_fp, ignore_index=True)
+X_data_ML = pd.concat([X_data_use, X_data_fp], axis=1, join='inner')
+
+Y_data_ML = Y_data.copy()
+
+#>>>  SHOW X_data_fp, Y_data_fp
+
+# %% Train test split
+
+X_train_ML, X_test_ML, y_train_ML, y_test_ML = train_test_split(X_data_ML, Y_data_ML,test_size=0.25,random_state=42)
+
+X_train = X_train_ML.copy().drop(columns = {"SMILES", "molecule", "morgan_fp"})
+X_test = X_test_ML.copy().drop(columns = {"SMILES", "molecule", "morgan_fp"})
+x_total = X_data_ML.copy().drop(columns = {"SMILES", "molecule", "morgan_fp"})
+
+y_train = y_train_ML.copy()
+y_test = y_test_ML.copy()
+y_total = Y_data_ML.copy()
+
+# >>> EXAMPLE SHOW {X, Y} of {TRAIN TEST TOTAL}
+
+# %%
+# Modeling
+
+
+Linear = LinearRegression()
+Linear.fit(X_train, y_train)
+
+
+Score_table = Scoring(Linear, X_train, X_test, x_total, y_train, y_test, y_total)
+Export(Score_table, "TESTFILE.csv")
