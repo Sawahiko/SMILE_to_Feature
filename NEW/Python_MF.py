d3d0725a7956a628cd2c1f93d9d92cab9938f894
Sawahiko <93868260+Sawahiko@users.noreply.github.com>
2023-10-15 21:54:58 +0700

    CHON B-MF
    change r and bit
    RF, Ridge, CB
    remain : XGB (running)

--- NEW/Python_MF.py
+++ NEW/Python_MF.py
@@ -21,58 +21,94 @@ from Python_Scoring_Export import Scoring, Export
 
 from Python_MLModel import RF, Ridge_M, XGB, CB
 
+old_df = pd.DataFrame({
+    'MAE':[0], 'MAPE(%)':[0], 'RMSE':[0], 'R2':[0], 'Radius':[0], 'nBits':[0], 'Model':[0]
+    })
 
-# %% Option
-MF_bit = 2**10
-MF_radius = 6
+# =============================================================================
+# # %% Option
+# MF_bit = 2**5
+# MF_radius = 6
+# Name_model = "CB"
+# =============================================================================
 
-# %% Import Data : 560 datapoint
-# Import Data
-df = pd.read_excel("../DataTb.xlsx",sheet_name="AllDataSet")
+# %% Option Many Bit
+MF_bit_s = [2**8-1, 2**9-1, 2**10-1, 2**11-1, 2**12-1, 2**13-1]
+MF_radius_s = [2, 3, 4, 5, 6]
 
-# Select feature for data: X=SMILE, Y=Tb
-X_data_excel= df[["SMILES"]]
-Y_data= df["Tb"]
-
-# %% Data Preparation
-# Generate Fingerprint from SMILE
-
-X_data_use = X_data_excel.copy()
-X_data_use["molecule"] = X_data_use["SMILES"].apply(lambda x: Chem.MolFromSmiles(x))
-X_data_use["morgan_fp"] = X_data_use["molecule"].apply(lambda x: rdMolDescriptors.GetMorganFingerprintAsBitVect(
-        x, 
-        radius=MF_radius, 
-        nBits=MF_bit, 
-        useFeatures=True, useChirality=True))
-
-# Transfrom Fingerprint to Column in DataFrame
-X_data_fp = []
-for i in range(X_data_use.shape[0]):
-    #print(np.array(X_data_use["morgan_fp"][i]))
-    array = np.array(X_data_use["morgan_fp"][i])
-    datafram_i = pd.DataFrame(array)
-    datafram_i = datafram_i.T
-    X_data_fp.append(datafram_i)
-x_data_fp = pd.concat(X_data_fp, ignore_index=True)
-
-y_data_fp = Y_data.copy()
-
+#MF_bit_s = [2**5-1, 2**6-1]
+#MF_radius_s = [2]
+Name_model = "CB"
+j=0
+for MF_radius in MF_radius_s:
+    for MF_bit in MF_bit_s :
+        
+        # %% 
+        # Import Data
+        df = pd.read_excel("../Data.xlsx",sheet_name="Load_AllDataSetC12")
+        
+        # Select feature for data: X=SMILE, Y=Tb
+        X_data_excel= df[["SMILES"]]
+        Y_data= df["Tb"]
+        
+        # %% Data Preparation
+        # Generate Fingerprint from SMILE
+        
+        X_data_use = X_data_excel.copy()
+        X_data_use["molecule"] = X_data_use["SMILES"].apply(lambda x: Chem.MolFromSmiles(x))
+        X_data_use["morgan_fp"] = X_data_use["molecule"].apply(lambda x: rdMolDescriptors.GetMorganFingerprintAsBitVect(
+                x, 
+                radius=MF_radius, 
+                nBits=MF_bit, 
+                useFeatures=True, useChirality=True))
+        
+        # Transfrom Fingerprint to Column in DataFrame
+        X_data_fp = []
+        for i in range(X_data_use.shape[0]):
+            #print(np.array(X_data_use["morgan_fp"][i]))
+            array = np.array(X_data_use["morgan_fp"][i])
+            datafram_i = pd.DataFrame(array)
+            datafram_i = datafram_i.T
+            X_data_fp.append(datafram_i)
+        x_data_fp = pd.concat(X_data_fp, ignore_index=True)
+        
+        y_data_fp = Y_data.copy()
+        
+        # %%
+        # Train-test_Modeling & Cross Validation Modeling
+        
+        
+        x_train_fp, x_test_fp, y_train_fp, y_test_fp = train_test_split(x_data_fp, y_data_fp,
+                                                                        test_size=0.20,
+                                                                        random_state=42)
+        start_time = time.time()
+        model = CB(x_train_fp, y_train_fp)
+        
+        end_time = time.time()
+        print("Training Elasped Time : ", end_time-start_time, " seconds")
+        
+        
+        # %%
+        # Scoring & Export
+        Score_table = Scoring(model , x_train_fp, x_test_fp, x_data_fp, y_train_fp, y_test_fp, y_data_fp)
+        #Export(Score_table, "2023-10-10 MF1024_XGB r=6.csv")
+        
+        
+        # %%
+        
+        df2 = pd.DataFrame({'Radius': [f"r= {MF_radius}", f"r= {MF_radius}", f"r= {MF_radius}"], 
+                            'nBits': [f"n= {MF_bit}", f"n= {MF_bit}", f"n= {MF_bit}"],
+                            'Model': [Name_model, Name_model, Name_model]
+                                      })
+        
+        df = pd.concat([Score_table, df2], axis=1)
 # %%
-# Train-test_Modeling & Cross Validation Modeling
+        if(j>0):
+            old_df = df_combine.copy()
+        new_df = df.copy()
+        j+=1
 
-
-x_train_fp, x_test_fp, y_train_fp, y_test_fp = train_test_split(x_data_fp, y_data_fp,
-                                                                test_size=0.20,
-                                                                random_state=42)
-start_time = time.time()
-Ridge_model = XGB(x_train_fp, y_train_fp)
-
-end_time = time.time()
-print("Elasped Time : ", end_time-start_time, " seconds")
-
-
+        df_combine = pd.concat([old_df, new_df], ignore_index=True)
+        
 # %%
-# Scoring & Export
-Score_table = Scoring(Ridge_model , x_train_fp, x_test_fp, x_data_fp, y_train_fp, y_test_fp, y_data_fp)
-#Export(Score_table, "2023-10-10 MF1024_XGB r=6.csv")
-
+Export(df_combine, "B-MF 2023-10-15/CB.csv")
