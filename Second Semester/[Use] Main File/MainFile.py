5e302bb044e378f0e9f1c46d2431c5d09ff7b6bd
Sawahiko <93868260+Sawahiko@users.noreply.github.com>
2023-12-11 19:39:55 +0700

    Psat custom loss func done

--- Second Semester/[Use] Main File/MainFile.py
+++ Second Semester/[Use] Main File/MainFile.py
@@ -19,7 +19,7 @@ from rdkit.Chem import AllChem
 from rdkit import DataStructs
 
 # Our module
-from Python_Scoring_Export import Scoring, Export
+#from Python_Scoring_Export import Scoring, Export
 
 #%%
 
@@ -66,32 +66,87 @@ y_data_fp = Y_data.copy()
 #%%
 x_train, x_test, y_train, y_test = train_test_split(x_data_fp, y_data_fp,test_size=0.2,random_state=42)
 
-#%%
-# Create Nueron Network Model
-model = Sequential()
-# Add BatchNormalization after each dense layer
-model.add(Dense(500, input_dim=x_train.shape[1], activation='relu'))
-model.add(Dense(100, activation='relu'))
-model.add(Dense(3))
-model.compile(optimizer='adam', loss='mean_squared_error')
-model.fit(x_train, y_train, epochs=50, batch_size=16, validation_split=0.2)
+# %%
+
+import keras.backend as K
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras.losses import mse
+import numpy as np
+
+# Some random training data
+labels_1 = y_train["A"]
+labels_2 = y_train["B"]
+labels_3 = y_train["C"]
+
+# Input layer, one hidden layer
+input_layer = Input((x_train.shape[1],))
+dense_1 = Dense(500, "relu")(input_layer)
+dense_2 = Dense(100, "relu")(dense_1)
+# Two outputs
+output_1 = Dense(1)(dense_2)
+output_2 = Dense(1)(dense_2)
+output_3 = Dense(1)(dense_2)
+
+# Two additional 'inputs' for the labels
+label_layer_1 = Input((1,))
+label_layer_2 = Input((1,))
+label_layer_3 = Input((1,))
+
+# Instantiate model, pass label layers as inputs
+model = Model(inputs=[input_layer, label_layer_1, label_layer_2, label_layer_3], outputs=[output_1, output_2, output_3])
+
+# Construct your custom loss as a tensor
+# =============================================================================
+# def loss(output_1, label_layer_1, output_2, label_layer_2) :
+#     return K.mean(mse(output_1, label_layer_1) * mse(output_2, label_layer_2))
+# =============================================================================
+def Psat_cal(T,A,B,C):
+    return np.log1p(A+(B/(T+C)))
+
+def Psat_cal_TF(T,A,B,C):
+    return A+(B/(T+C))
+
+Temp = 300
+#loss_fun = K.mean(mse(output_1, label_layer_1) * mse(output_2, label_layer_2) * mse(output_3, label_layer_3))
+loss_fun = K.mean(mse(Psat_cal_TF(Temp, output_1, output_2, output_3), Psat_cal_TF(Temp, label_layer_1, label_layer_2, label_layer_3)))
+
+# Add loss to model
+model.add_loss(loss_fun)
+
+# Compile without specifying a loss
+model.compile(optimizer='adam')
+
+dummy = np.zeros(x_train.shape[0])
+model.fit([x_train, labels_1, labels_2, labels_3], dummy, epochs=100)
+
 
 #%%
-ABC_predict = model.predict(x_test)
-
+A_actual = y_test["A"]
+B_actual = y_test["B"]
+C_actual = y_test["C"]
+ABC_predict = model.predict([x_test, A_actual, B_actual, C_actual])
+ABC_predict = np.dstack(ABC_predict).reshape(x_test.shape[0],3)
+                             
 A_predict = ABC_predict[:,0]
 B_predict = ABC_predict[:,1]
 C_predict = ABC_predict[:,2]
 
-A_actual = y_test["A"]
-B_actual = y_test["B"]
-C_actual = y_test["C"]
+Psat_predict = Psat_cal(Temp , A_predict, B_predict, C_predict)
+Psat_antione = Psat_cal(Temp , A_actual, B_actual, C_actual)
 
-def Psat_cal(T,A,B,C):
-    return np.log10(A+(B/(T+C)))
 
-Psat_predict = Psat_cal(100, A_predict, B_predict, C_predict)
-Psat_antione = Psat_cal(100, A_actual, B_actual, C_actual)
+# =============================================================================
+# plt.xlim(x_min, x_max)
+# plt.ylim(y_min, y_max)
+# =============================================================================
+
+x_min, x_max = 2, 4.5
+y_min, y_max = 2, 4.5
+x = np.linspace(x_min, x_max, 100)
+y = x
+plt.plot(x, y, color='black',linestyle='dashed', label='x=y')
 
 plt.scatter(Psat_predict, Psat_antione)
+
 
