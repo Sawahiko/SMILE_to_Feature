0cec27fecda223e7540ce3c7cfe0a77f0093fd80
Sawahiko <93868260+Sawahiko@users.noreply.github.com>
2024-03-07 10:46:10 +0700

    refactor#3

--- Second Semester/Refactor Code/py_03-1 Psat-ML.py
+++ Second Semester/Refactor Code/py_03-1 Psat-ML.py
@@ -1,37 +1,22 @@
 # Python
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import time
 from datetime import datetime
-import random
 
 # Machine Learning
 ## Algorithm
-from sklearn.ensemble import RandomForestRegressor
-from xgboost import XGBRegressor
-## Tool, Error Metric
-from sklearn.model_selection import RandomizedSearchCV, KFold, cross_validate
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error, mean_squared_error, r2_score
-from joblib import dump, load
-
-# RDKit
-from rdkit import Chem
-from rdkit.Chem import Descriptors
-from rdkit.ML.Descriptors import MoleculeDescriptors
-from rdkit.Chem import rdMolDescriptors
-from rdkit.Chem import AllChem
-from rdkit import DataStructs
-
-
-#%% Default
 from sklearn.linear_model import LinearRegression, Ridge, Lasso
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.svm import SVR
+
+## Tool, Error Metric
+from sklearn.model_selection import KFold
+from joblib import dump, load
+
+#%% Default
 
 def Linear_default(x_train, y_train):
   lm = LinearRegression()
@@ -88,7 +73,7 @@ def RF(x_train, y_train):
     rf = RandomForestRegressor(random_state=42)
     kfold = KFold(n_splits=5, shuffle=True, random_state=42)  # Adjust number of splits as needed
 
-    grid_search = GridSearchCV(rf, param_grid=param_grid, cv=kfold, scoring='neg_mean_squared_error')
+    grid_search = GridSearchCV(rf, param_grid=param_grid, cv=kfold, scoring='neg_mean_squared_error', verbose=2)
     grid_search.fit(x_train, y_train)
 
     best_model = grid_search.best_estimator_
@@ -108,7 +93,7 @@ def XGB(x_train, y_train):
     xgb = XGBRegressor(random_state=42)
     kfold = KFold(n_splits=5, shuffle=True, random_state=42)
 
-    grid_search = GridSearchCV(xgb, param_grid=param_grid, cv=kfold, scoring="neg_root_mean_squared_error")
+    grid_search = GridSearchCV(xgb, param_grid=param_grid, cv=kfold, scoring="neg_root_mean_squared_error", verbose=2)
     grid_search.fit(x_train, y_train)
 
     best_model = grid_search.best_estimator_
@@ -148,7 +133,7 @@ def KNN(x_train, y_train):
     knn = KNeighborsRegressor()
     kfold = KFold(n_splits=5, shuffle=True, random_state=42)
 
-    grid_search = GridSearchCV(knn, param_grid=param_grid, cv=kfold)
+    grid_search = GridSearchCV(knn, param_grid=param_grid, cv=kfold, verbose=2)
     grid_search.fit(x_train, y_train)
 
     best_model = grid_search.best_estimator_
@@ -169,7 +154,7 @@ def SVM_M(x_train, y_train):
     svr = SVR()
     kfold = KFold(n_splits=5, shuffle=True, random_state=42)
 
-    grid_search = GridSearchCV(svr, param_grid=param_grid, cv=kfold, scoring='neg_mean_squared_error')
+    grid_search = GridSearchCV(svr, param_grid=param_grid, cv=kfold, scoring='neg_mean_squared_error', verbose=2)
     grid_search.fit(x_train, y_train)
 
     best_model = grid_search.best_estimator_
@@ -181,16 +166,16 @@ x_train = pd.read_csv("csv_02-3 std_x_train.csv").iloc[:,1:]
 y_train = pd.read_csv("csv_02-4 std_y_train.csv").iloc[:,1]
 x_test  = pd.read_csv("csv_02-5 std_x_test.csv").iloc[:,1:]
 y_test  = pd.read_csv("csv_02-6 std_y_test.csv").iloc[:,1]
-
+#%%
 scaler_x = load("file_02-1 scaler_x.joblib")
-scaler_y = load("file_02-2 scaler_y.joblib)
+scaler_y = load("file_02-2 scaler_y.joblib")
 
 #%% Grid Training
 
 # Specified model need to run
 #names_bestpar = ["DT", "RF", "XGB", "KNN"]
-names_bestpar = ["DT"]
-models_bestpar = [DT]
+names_bestpar = ["DT", "RF"]
+models_bestpar = [DT, RF]
 
 # Run Training Model
 all_result_model_bestpar = []
@@ -207,51 +192,11 @@ for iteration in range(len(names_bestpar)) :
     all_time_fitting_bestpar.append(duration)
 
 #%%
-from datetime import datetime
-
-
-func_name = ["MAE", "RMSE", "R2"]
-rmse_func = lambda x,y : mean_squared_error(x,y, squared=False)
-func_list = [mean_absolute_error, rmse_func, r2_score]
-def create_result_from_predict_value(table_predict, func_name, func_list):
-    list_df = list()
-    name_method = pd.Series(table_predict.apply(lambda x : x[0], axis=1), name="Method")
-    df_1 = name_method.to_frame();   list_df.append(df_1)
-    for i in range(len(func_list)):
-      list_error_metric = []
-
-      name_fx = func_name[i]
-      func    = func_list[i]
-      #name_method = pd.Series(table_predict.apply(lambda x : x[0], axis=1), name="Method")
-      value = pd.Series(table_predict.apply(lambda x : func(x[1], x[2]), axis=1),name=name_fx)
-      df_2=value.to_frame()
-      #print(df_1)
-      #print(df_2)
-      #df_combine = pd.merge(df_1, df_2, how='inner', left_index=True, right_index=True)
-      #df_combine = pd.concat([df_1, df_2], axis=1)
-      list_df.append(df_2)
-
-    return pd.concat(list_df, axis=1)
-
-
-
-#print(create_result_from_predict_value(result_train, func_name, func_list))
-print("\n")
-#print(create_result_from_predict_value(result_test, func_name, func_list))
-
 def model_assess(X_train, X_test, y_train, y_test,
                  scaler_x, scaler_y,
                  list_model, name_model,
                  all_time_fitting, title = "Default"):
 
-    func_name = ["MAE", "RMSE", "R2"]
-    rmse_func = lambda x,y : mean_squared_error(x,y, squared=False)
-    func_list = [mean_absolute_error, rmse_func, r2_score]
-    
-    model_assess_table = pd.DataFrame(['Method','Training MAE', 'Training RMSE','Training R2','Test MAE', 'Test RMSE','Test R2', 'Time Evaluate']).transpose()
-    new_header = model_assess_table.iloc[0] #grab the first row for the header
-    model_assess_table.columns = new_header #set the header row as the df header
-    model_assess_table.drop(index=model_assess_table.index[0], axis=0, inplace=True)
     
     train_prediction_table = pd.DataFrame(['Method','Training Predict','Training Actual']).transpose()
     new_header = train_prediction_table.iloc[0] #grab the first row for the header
@@ -270,7 +215,7 @@ def model_assess(X_train, X_test, y_train, y_test,
         name = name_model[iteration]
     
         if("DL" not in name):
-          model_train.fit(X_train, y_train)
+          #model_train.fit(X_train, y_train)
           y_train_pred = model_train.predict(X_train)
           y_test_pred  = model_train.predict(X_test)
         else:
@@ -280,17 +225,10 @@ def model_assess(X_train, X_test, y_train, y_test,
         duration = (time_end - time_start).total_seconds()
         print(name)
         print(duration)
-    
-        train_mae = mean_absolute_error(y_train, y_train_pred)
-        train_rmse = mean_squared_error(y_train, y_train_pred, squared=False)
-        train_r2 = r2_score(y_train, y_train_pred)
-        test_mae = mean_absolute_error(y_test, y_test_pred)
-        test_rmse = mean_squared_error(y_test, y_test_pred, squared=False)
-        test_r2 = r2_score(y_test, y_test_pred)
-    
-        results = pd.DataFrame([name_model[iteration],train_mae, train_rmse, train_r2, test_mae, test_rmse, test_r2, duration]).transpose()
-        results.columns = ['Method','Training MAE', 'Training RMSE','Training R2','Test MAE', 'Test RMSE','Test R2', 'Time Evaluate']
-    
+        y_train = scaler_y.inverse_transform(np.array(y_train).reshape(-1,1)).flatten()
+        y_train_pred = scaler_y.inverse_transform(np.array(y_train_pred).reshape(-1,1)).flatten()
+        y_test = scaler_y.inverse_transform(np.array(y_test).reshape(-1,1)).flatten()
+        y_test_pred = scaler_y.inverse_transform(np.array(y_test_pred).reshape(-1,1)).flatten()
     
         train_prediction_result = pd.DataFrame([name_model[iteration],y_train_pred, y_train]).transpose()
         train_prediction_result.columns = ['Method','Training Predict','Training Actual']
@@ -298,20 +236,19 @@ def model_assess(X_train, X_test, y_train, y_test,
         test_prediction_result = pd.DataFrame([name_model[iteration], y_test_pred, y_test]).transpose()
         test_prediction_result.columns = ['Method','Test Predict','Test Actual']
     
-        model_assess_table = pd.concat([model_assess_table, results])
         train_prediction_table = pd.concat([train_prediction_table, train_prediction_result])
         test_prediction_table = pd.concat([test_prediction_table, test_prediction_result])
-    model_assess_table['Time Training'] = all_time_fitting
-    return model_assess_table, train_prediction_table, test_prediction_table
+    return train_prediction_table, test_prediction_table
 
+#%% Prediction
+train_prediction_bestpar_original, test_prediction_bestpar_original = model_assess(x_train, x_test,
+                                                        y_train, y_test,
+                                                        scaler_x, scaler_y,
+                                                        all_result_model_bestpar, names_bestpar,
+                                                        all_time_fitting_bestpar)
 
-
-
-
-
-
-
-
+train_predict_table = train_prediction_bestpar_original.explode(["Training Predict", "Training Actual"]).reset_index(drop=True)
+test_predict_table = test_prediction_bestpar_original.explode(["Test Predict", "Test Actual"]).reset_index(drop=True)
 
 #%% Export
 
@@ -322,7 +259,8 @@ for i in range(len(names_bestpar)):
     print(path_model)
     dump(all_result_model_bestpar[i], path_model)
     
-# Export Evaluation
-
-
 # Export Prediction Table
+train_predict_table.to_csv("csv_03-1-1 Predict Table - train DT.csv")
+test_predict_table.to_csv("csv_03-1-2 Predict Table - test DT.csv")
+
+# Export Evaluation
