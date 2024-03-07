5844bafaaf694efe16e848911f1e0f965cdf4dc1
Sawahiko <93868260+Sawahiko@users.noreply.github.com>
2024-03-07 09:28:56 +0700

    refactor#1

---
+++ Second Semester/Refactor Code/py_01 Data Cleansing.py
@@ -0,0 +1,172 @@
+# Python
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import random
+
+# RDKit
+from rdkit import Chem
+from rdkit.Chem import Descriptors
+from rdkit.ML.Descriptors import MoleculeDescriptors
+from rdkit.Chem import rdMolDescriptors
+from rdkit.Chem import AllChem
+from rdkit import DataStructs
+
+#%%
+def remove_outliers_boxplot(Excel_path, Excel_sheetname, columns, IQR_factor=1.5, show_result=False):
+    df = pd.read_excel(Excel_path, sheet_name=Excel_sheetname)
+
+    for column in columns:
+        
+        plt.title(column)
+        plt.boxplot(df[column])
+        plt.show()
+        
+        Q1 = df[column].quantile(0.25)
+        Q3 = df[column].quantile(0.75)
+        IQR = Q3 - Q1
+
+        lower_bound = Q1 - (IQR_factor * IQR)
+        upper_bound = Q3 + (IQR_factor * IQR)
+        
+        df = df[~((df[column] < lower_bound) | (df[column] > upper_bound))]
+        if show_result:
+            name_title = "Extracted "+column 
+            plt.title(name_title)
+            plt.boxplot(df[column])
+            plt.show()
+        
+
+    return df.reset_index(drop=True)
+
+#%% Remove A B C Tmin Tmax Outliner
+df_original_remove_ABCMinMax = remove_outliers_boxplot("../[Use] Data Preparation/Psat_AllData_1.xlsx",
+                                                 "AllDataSet",
+                                                 ["A", "B", "C", "Tmin", "Tmax"],
+                                                 1.5,True)
+#%%
+def get_all_atomic_number(SMILES):
+    def composition(molecule):
+        """Get the composition of an RDKit molecule:
+        Atomic counts, including hydrogen atoms, and any charge.
+        For example, fluoride ion (chemical formula F-, SMILES string [F-])
+        returns {9: 1, 0: -1}.
+    
+        :param molecule: The molecule to analyze
+        :type some_input: An RDKit molecule
+        :rtype: A dictionary.
+        """
+        #print(SMILES)
+        set_atom = set()
+        # Check that there is a valid molecule
+        if molecule:
+    
+            # Add hydrogen atoms--RDKit excludes them by default
+            molecule_with_Hs = Chem.AddHs(molecule)
+    
+            # Get atomic number
+            for atom in molecule_with_Hs.GetAtoms():
+                set_atom.add(atom.GetAtomicNum())
+            return set_atom
+
+    m = Chem.MolFromSmiles(SMILES)
+    try:
+        newlist = list(composition(m))
+        newlist.sort()
+        return newlist
+    except:
+        return list()
+    
+#%% Get CHON Scope
+df2 = df_original_remove_ABCMinMax[["SMILES"]].copy()
+df2["Atom"] = df2["SMILES"].apply(lambda x: get_all_atomic_number(x))
+df2["Count Unique Atom"] = df2["Atom"].apply(lambda x: len(x))
+def scopeCHON (list_atom_number):
+    return all( x in [1,6,7,8] for x in list_atom_number)
+df2["Pass Screen"] = df2["Atom"].apply(lambda x: scopeCHON(x))
+df3 = df2.loc[df2["Pass Screen"].isin([True])]
+df3 = df3.drop(columns="SMILES", axis=0)
+df3_index = list(df3.index)
+
+#%% Change Scope Letter
+
+def cb(row):
+    #print(row)
+    if row == [1,6]:
+        return "CH"
+    elif row == [1,6,8]:
+        return "CHO"
+    elif row == [1,6,7]:
+        return "CHN"
+    elif row == [1,6,7,8]:
+        return "CHON"
+    elif row == [6,7]:
+        return "CN"
+    elif row == [6,8]:
+        return "CO"
+    elif row == [6,7,8]:
+        return "CON"
+df3["Atom2"] = df3["Atom"].apply(lambda x : cb(x))
+#%% Get ABC TMin Tmax CHON
+df_prepare = df_original_remove_ABCMinMax.copy()
+df_prepare = df_prepare.filter(items = df3_index, axis=0)
+#df_prepare.join(df3, on="SMILES")
+df_ABCMinMax_CHON = pd.concat([df_prepare, df3], axis=1)
+df_ABCMinMax_CHON = df_ABCMinMax_CHON[df_ABCMinMax_CHON["Count Unique Atom"]>0]
+
+#%% Get C1-C12, Unique SMILES name
+df_export = df_ABCMinMax_CHON[df_ABCMinMax_CHON["No.C"]<=12]
+df_export = df_export[df_export["No.C"]>0]
+df_export = df_export.drop_duplicates(subset=['SMILES'])
+df_export = df_export.drop_duplicates(subset=['Name'])
+
+
+#%% Generate 5 Temp
+df1 = df_export.copy().reset_index(drop=True)
+def generate_points(row, amount_point):
+    start = row["Tmin"]; end = row["Tmax"];
+    range_temp = end-start
+    if range_temp>5:
+        return np.linspace(start, end, amount_point)
+    else:
+        return start
+df1["T"] = df1.apply(lambda x : generate_points(x, 5), axis=1)
+
+df1 = df1.explode('T')
+df1['T'] = df1['T'].astype('float32')
+df1 = df1.reset_index(drop=True)
+
+# Generate VP from Antione Coeff and Temp
+def Psat_cal(T,A,B,C):
+    #return pow(A-(B/(T+C)),10)/(10^(3))
+    return A-(B/(T+C))
+
+df1["Vapor_Presssure"] = Psat_cal(df1["T"], df1["A"], df1["B"], df1["C"])
+
+
+plt.title("Log(Psat)")
+plt.boxplot(df1["Vapor_Presssure"])
+plt.show()
+#%%
+column = "Vapor_Presssure"
+Q1 = df1[column].quantile(0.25)
+Q3 = df1[column].quantile(0.75)
+IQR = Q3 - Q1
+IQR_factor = 1.5
+lower_bound = Q1 - (IQR_factor * IQR)
+upper_bound = Q3 + (IQR_factor * IQR)
+
+df01 = df1[~((df1[column] < lower_bound) | (df1[column] > upper_bound))]
+
+plt.title("Extracted Log(Psat)")
+plt.boxplot(df01["Vapor_Presssure"])
+plt.show()
+
+#%% Summary Output
+df_Psat_noOut = df01.copy()
+
+filter2 = df_export["SMILES"].isin(df01["SMILES"].drop_duplicates())
+df02 = df_export[filter2]
+
+df_export.to_csv("csv_01 Psat_[X]_ABCTminTmaxC1-12.csv")
+#df02.to_csv("Psat_NO_ABCTminTmaxC1-12Psat.csv")
---
+++ Second Semester/Refactor Code/py_02 Data Preparation Engineering Split.py
@@ -0,0 +1,168 @@
+# Python
+import numpy as np
+import pandas as pd
+
+# Tool, Error Metric
+from sklearn.model_selection import train_test_split
+
+# RDKit
+from rdkit import Chem
+from rdkit.Chem import rdMolDescriptors
+from rdkit import DataStructs
+
+
+#%% Import Data
+df = pd.read_csv("csv_01 Psat_[X]_ABCTminTmaxC1-12.csv")
+
+# New Train-Test Split
+train, test = train_test_split(df, test_size=0.2, random_state=42)
+
+#%% 
+# Genearate Temp in Tmin-Tmax and expand
+df1 = train.copy()
+    ## Function to generate equally distributed points
+def generate_points(row, amount_point):
+    start = row["Tmin"]; end = row["Tmax"];
+    return np.linspace(start, end, amount_point)
+df1["T"] = df1.apply(lambda x : generate_points(x, 5), axis=1)
+
+df1  = df1.explode('T')
+df1['T'] = df1['T'].astype('float32')
+df1 = df1.reset_index(drop=True)
+
+# Generate VP from Antione Coeff and Temp
+def Psat_cal(T,A,B,C):
+    #return pow(A-(B/(T+C)),10)/(10^(3))
+    return A-(B/(T+C))
+
+df1["Vapor_Presssure"] = Psat_cal(df1["T"], df1["A"], df1["B"], df1["C"])
+#test = df1[df1["SMILES"].isin(df2[df2["Vapor_Presssure"] <-20]["SMILES"])]
+#print(test[['SMILES','Vapor_Presssure']].sort_values(by="Vapor_Presssure").head(5))
+#print(f"\n {test[['SMILES','Vapor_Presssure']].index} ")
+# Get Needed Table and split for Training
+df2 = df1[["SMILES", "T", "Vapor_Presssure"]]
+df2 = df2[~df2["SMILES"].isin(df2[df2["Vapor_Presssure"] <-20]["SMILES"])].reset_index()
+#print(df2[['SMILES','Vapor_Presssure']].sort_values(by="Vapor_Presssure").head(5))
+X_data= df2[["SMILES"]]               # feature: SMILE, T
+Y_data= df2[["Vapor_Presssure"]]        # Target : Psat
+print(df2.sort_values(by="Vapor_Presssure"))
+
+# Fingerprint
+# Parameter for Generate Morgan Fingerprint
+MF_radius = 3;   MF_bit = 2048
+
+# Generate Fingerprint from SMILE
+X_data_use = X_data.copy()
+X_data_use["molecule"] = X_data_use["SMILES"].apply(lambda x: Chem.MolFromSmiles(x))    # Create Mol object from SMILES
+X_data_use["count_morgan_fp"] = X_data_use["molecule"].apply(lambda x: rdMolDescriptors.GetHashedMorganFingerprint(
+    x,
+    radius=MF_radius,
+    nBits=MF_bit,
+    useFeatures=True, useChirality=True))         # Create Morgan Fingerprint from Mol object
+
+
+# Transfrom Fingerprint to Datafrme that we can use for training
+X_data_use["arr_count_morgan_fp"] = 0
+X_data_fp = []
+for i in range(X_data_use.shape[0]):
+    blank_arr = np.zeros((0,), dtype=np.int8)
+    DataStructs.ConvertToNumpyArray(X_data_use["count_morgan_fp"][i],blank_arr)
+    datafram_i = pd.DataFrame(blank_arr)
+    datafram_i = datafram_i.T
+    X_data_fp.append(datafram_i)
+x_data_fp = pd.concat(X_data_fp, ignore_index=True)
+x_data_fp = x_data_fp.astype(np.float32)
+
+# Final Data for Training
+x_data_fp[MF_bit] = df2["T"]      # Input  = Fingerprint + Temp
+y_data_fp = Y_data.copy()         # Output = Vapor Pressure
+
+x_train_notz = x_data_fp.copy()
+y_train_notz = np.ravel(y_data_fp.copy())
+
+#%%
+# Genearate Temp in Tmin-Tmax and expand
+df1 = test.copy()
+    ## Function to generate equally distributed points
+def generate_points(row, amount_point):
+    start = row["Tmin"]; end = row["Tmax"];
+    return np.linspace(start, end, amount_point)
+df1["T"] = df1.apply(lambda x : generate_points(x, 5), axis=1)
+
+df1  = df1.explode('T')
+df1['T'] = df1['T'].astype('float32')
+df1 = df1.reset_index(drop=True)
+#print(test)
+# Generate VP from Antione Coeff and Temp
+def Psat_cal(T,A,B,C):
+    #return pow(A-(B/(T+C)),10)/(10^(3))
+    return A-(B/(T+C))
+
+df1["Vapor_Presssure"] = Psat_cal(df1["T"], df1["A"], df1["B"], df1["C"])
+
+# Get Needed Table and split for Training
+df2 = df1[["SMILES", "T", "Vapor_Presssure"]]
+df2 = df2[~df2["SMILES"].isin(df2[df2["Vapor_Presssure"] <-20]["SMILES"])].reset_index()
+X_data= df2[["SMILES"]]               # feature: SMILE, T
+Y_data= df2[["Vapor_Presssure"]]        # Target : Psat
+#df[df["SMILES"].isin(df2[df2["Vapor_Presssure"] <-20]["SMILES"])]
+print(df2.sort_values(by="Vapor_Presssure"))
+
+# Fingerprint
+# Parameter for Generate Morgan Fingerprint
+MF_radius = 3;   MF_bit = 2048
+
+# Generate Fingerprint from SMILE
+X_data_use = X_data.copy()
+X_data_use["molecule"] = X_data_use["SMILES"].apply(lambda x: Chem.MolFromSmiles(x))    # Create Mol object from SMILES
+X_data_use["count_morgan_fp"] = X_data_use["molecule"].apply(lambda x: rdMolDescriptors.GetHashedMorganFingerprint(
+    x,
+    radius=MF_radius,
+    nBits=MF_bit,
+    useFeatures=True, useChirality=True))         # Create Morgan Fingerprint from Mol object
+
+# Transfrom Fingerprint to Datafrme that we can use for training
+X_data_use["arr_count_morgan_fp"] = 0
+X_data_fp = []
+for i in range(X_data_use.shape[0]):
+    blank_arr = np.zeros((0,), dtype=np.int8)
+    DataStructs.ConvertToNumpyArray(X_data_use["count_morgan_fp"][i],blank_arr)
+    datafram_i = pd.DataFrame(blank_arr)
+    datafram_i = datafram_i.T
+    X_data_fp.append(datafram_i)
+x_data_fp = pd.concat(X_data_fp, ignore_index=True)
+x_data_fp = x_data_fp.astype(np.float32)
+
+# Final Data for Training
+x_data_fp[MF_bit] = df2["T"]      # Input  = Fingerprint + Temp
+y_data_fp = Y_data.copy()         # Output = Vapor Pressure
+
+x_test_notz = x_data_fp.copy()
+y_test_notz = np.ravel(y_data_fp.copy())
+
+#%%
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+scale_x= StandardScaler()
+scale_x.fit(x_train_notz)
+print(scale_x)
+scale_y= StandardScaler()
+scale_y.fit(y_train_notz.reshape(-1,1))
+print(scale_y)
+x_train_fp = scale_x.transform(x_train_notz)
+x_test_fp  = scale_x.transform(x_test_notz)
+
+y_train_fp = scale_y.transform(y_train_notz.reshape(-1,1)).flatten()
+y_test_fp  = scale_y.transform(y_test_notz.reshape(-1,1)).flatten()
+
+#%% Export Section
+from joblib import dump, load
+#df2_train.to_csv("csv_02-1 df_train.csv")
+#df2_test.to_csv("csv_02-2 df_test.csv")
+
+pd.DataFrame(x_train_fp).to_csv("csv_02-3 std_x_train.csv")
+pd.DataFrame(y_train_fp).to_csv("csv_02-4 std_y_train.csv")
+pd.DataFrame(x_test_fp).to_csv("csv_02-5 std_x_test.csv")
+pd.DataFrame(y_test_fp).to_csv("csv_02-6 std_y_test.csv")
+
+dump(scale_x, "file_02-1 scaler_x.joblib")
+dump(scale_y, "file_02-2 scaler_y.joblib")
