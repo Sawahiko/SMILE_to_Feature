1232f9c344eac22271f4a2ca94664cb6c33c07db
oomsinsukit <oomsinsukit@gmail.com>
2024-03-15 11:35:33 +0700

    abc model

--- Second Semester/[Use] Main File/ABC_DL_Training.py
+++ Second Semester/[Use] Main File/ABC_DL_Training.py
@@ -11,17 +11,13 @@ from rdkit import DataStructs
 
 # Machine Learning
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, mean_absolute_percentage_error
 
 #Torch
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import TensorDataset, DataLoader, random_split
-import ray
-
-# Our module
-from Python_Scoring_Export import Scoring, Export
-from Python_RemoveO import remove_outliers, remove_outliers_boxplot
+from torch.utils.data import TensorDataset, DataLoader
 
 # %% Function
 
@@ -36,103 +32,95 @@ def generateTemp(Tmin, Tmax, amountpoint):
     return T_arr
 
 #%%
-df = pd.read_csv("./Psat_NO_ABCTminTmaxC1-12.csv")
+df = pd.read_csv("../Refactor Code/csv-01-0 Psat-1800.csv")
+
 df = df[df['SMILES'] != "None"].reset_index(drop=True)
-#df.to_csv('New_Data_Psat_Not_Outliers.csv')
 
 # Select feature for data: X=SMILE, Y=Tb
 X_data_excel= df[["SMILES"]]
 Y_data= df[["A","B","C"]]
-        
-df1 = df.copy()
-T_Test = generateTemp(df1["Tmin"], df1["Tmax"], 5)
-T_all = []
-for i in range(len(T_Test[0])):
-    T_gen_x_point = [item[i] for item in T_Test]
-    T_all.append(T_gen_x_point)
-
-df1["T"] = T_all
-df1  = df1.explode('T')
-df1['T'] = df1['T'].astype('float32')
-df1.drop(df.columns[0], axis='columns', inplace=True)
-df1 = df1.reset_index(drop=True)
-
-Psat_test = Psat_cal(df1["T"], df1["A"], df1["B"], df1["C"])
-df1["Vapor_Presssure"] = Psat_test
-
-df2 = df1[["SMILES", "T", "A", "B", "C", "Vapor_Presssure"]]
-# Select feature for data: X=SMILE, Y=Tb
-#X_data_excel= df2[["SMILES","T"]]
-#Y_data= df2[["A", "B", "C"]]
-
-X_data_excel= df[["SMILES"]]
-Y_data= df[["A", "B", "C"]]
-#Y_data= df[["A"]]
-T_X_data_excel = df2[["T"]]
 
 # %% Fingerprint
 # Generate Fingerprint from SMILE
 MF_radius = 3
-MF_bit = 2048
+MF_bit = 4096
+train, test = train_test_split(df, test_size=0.2, random_state=42, stratify=df["Func. Group"])
 
-X_data_use = X_data_excel.copy()
-X_data_use["molecule"] = X_data_use["SMILES"].apply(lambda x: Chem.MolFromSmiles(x))
-X_data_use["count_morgan_fp"] = X_data_use["molecule"].apply(lambda x: rdMolDescriptors.GetHashedMorganFingerprint(
+X_train = train[['SMILES']]
+Y_train = train[['A','B','C']]
+
+X_test = test[['SMILES']]
+Y_test = test[['A','B','C']]
+
+X_train_use = X_train.copy()
+X_train_use["molecule"] = X_train_use["SMILES"].apply(lambda x: Chem.MolFromSmiles(x))
+X_train_use["count_morgan_fp"] = X_train_use["molecule"].apply(lambda x: rdMolDescriptors.GetHashedMorganFingerprint(
     x,
     radius=MF_radius,
     nBits=MF_bit,
     useFeatures=True, useChirality=True))
-X_data_use["arr_count_morgan_fp"] = 0
-#X_data_use["arr_count_morgan_fp"] = np.zeros((0,), dtype=np.int8)
+X_train_use["arr_count_morgan_fp"] = 0
 
-#X_data_use["arr_count_morgan_fp"]
-#new_df = X_data_use.apply(DataStructs.ConvertToNumpyArray, axis=0, args=('count_morgan_fp',))
+X_test_use = X_test.copy()
+X_test_use["molecule"] = X_test_use["SMILES"].apply(lambda x: Chem.MolFromSmiles(x))
+X_test_use["count_morgan_fp"] = X_test_use["molecule"].apply(lambda x: rdMolDescriptors.GetHashedMorganFingerprint(
+    x,
+    radius=MF_radius,
+    nBits=MF_bit,
+    useFeatures=True, useChirality=True))
+X_train_use["arr_count_morgan_fp"] = 0
+
+X_train_use = X_train_use.reset_index(drop=True)
+X_test_use  =X_test_use.reset_index(drop=True)
 
 # Transfrom Fingerprint to Column in DataFrame
-X_data_fp = []
-for i in range(X_data_use.shape[0]):
+X_data_train_fp = []
+for i in range(X_train_use.shape[0]):
     #print(np.array(X_data_use["morgan_fp"][i]))
     blank_arr = np.zeros((0,), dtype=np.int8)
-    DataStructs.ConvertToNumpyArray(X_data_use["count_morgan_fp"][i],blank_arr)
+    DataStructs.ConvertToNumpyArray(X_train_use["count_morgan_fp"][i],blank_arr)
     datafram_i = pd.DataFrame(blank_arr)
     datafram_i = datafram_i.T
-    X_data_fp.append(datafram_i)
-x_data_fp = pd.concat(X_data_fp, ignore_index=True)
-x_data_fp[MF_bit] = df2["T"]
-x_data_fp = x_data_fp.astype(np.float32)
+    X_data_train_fp.append(datafram_i)
+X_data_train_fp = pd.concat(X_data_train_fp, ignore_index=True)
+X_data_train_fp = X_data_train_fp.astype(np.float32)
 
-y_data_fp = Y_data.copy()
+X_data_test_fp = []
+for i in range(X_test_use.shape[0]):
+    #print(np.array(X_data_use["morgan_fp"][i]))
+    blank_arr = np.zeros((0,), dtype=np.int8)
+    DataStructs.ConvertToNumpyArray(X_test_use["count_morgan_fp"][i],blank_arr)
+    datafram_i = pd.DataFrame(blank_arr)
+    datafram_i = datafram_i.T
+    X_data_test_fp.append(datafram_i)
+X_data_test_fp = pd.concat(X_data_test_fp, ignore_index=True)
+X_data_test_fp = X_data_test_fp.astype(np.float32)
 
-#%% Train-test split
-# Spilt Data
-x_train_fp, x_test_fp, y_train_fp, y_test_fp = train_test_split(x_data_fp, y_data_fp,test_size=0.2,random_state=42)
+x_train = X_data_train_fp.copy()
+y_train = Y_train.copy()
 
-T_x_train = x_train_fp[[MF_bit]]; x_train = x_train_fp.drop(columns=MF_bit).astype(np.float64)
-T_x_test  = x_test_fp[[MF_bit]]; x_test = x_test_fp.drop(columns=MF_bit).astype(np.float64)
+x_test = X_data_test_fp.copy()
+y_test = Y_test.copy()
 
 # Normailzation
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()                     # created scaler
-scaler.fit(y_train_fp)                      # fit scaler on training dataset
-y_train = scaler.transform(y_train_fp)      # transform training dataset : y
-y_test = scaler.transform(y_test_fp)        # transform test dataset : y
-# =============================================================================
-# y_train = y_train_fp.values      # transform training dataset : y
-# y_test = y_test_fp.values        # transform test dataset : y
-# =============================================================================
+scaler.fit(y_train)                      # fit scaler on training dataset
+y_train = scaler.transform(y_train)      # transform training dataset : y
+y_test = scaler.transform(y_test)        # transform test dataset : y
 
-# Setup Data for DL
+#%%
+## Setup Data for DL
+# Train
 inputs = torch.tensor(x_train.values, dtype=torch.float64)
 labels = torch.tensor(y_train, dtype=torch.float64)
 trainloader = TensorDataset(inputs, labels)
 train_loader = DataLoader(trainloader, batch_size=32, shuffle=True)
+# Test
 inputs_test = torch.tensor(x_test.values, dtype=torch.float64)
 labels_test = torch.tensor(y_test, dtype=torch.float64)
 testloader = TensorDataset(inputs_test, labels_test)
 test_loader = DataLoader(trainloader, batch_size=32, shuffle=False)
-
-trainloader_id = ray.put(train_loader)
-testloader_id = ray.put(test_loader)
 
 #%% Training for prediction
 class ABC_2(nn.Module):
@@ -162,17 +150,13 @@ class ABC_2(nn.Module):
         return x
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = ABC_2(N_Input=MF_bit, N_Output=3, N_Hidden=1024, N_Layer=2, dropout_rate=0.2)
+model = ABC_2(N_Input=MF_bit, N_Output=3, N_Hidden=1000, N_Layer=3, dropout_rate=0.25)
 optimizer = optim.Adam(model.parameters(), lr=0.0001)
 criterion = nn.MSELoss()
 model.to(device)
 
-patience = 10  # Number of epochs to wait before stopping if no improvement
-
-best_val_loss = float('inf')
-epochs_no_improvement = 0
 training_log = {"train_loss": [], "val_loss": []}
-for epoch in range(400):  # loop over the dataset multiple times
+for epoch in range(200):  # loop over the dataset multiple times
 #    trainloader = ray.get(trainloader_id)
 #    testloader = ray.get(testloader_id)
     model.train()
@@ -211,20 +195,6 @@ for epoch in range(400):  # loop over the dataset multiple times
     training_log["val_loss"].append(val_loss)
     print(f'Epoch {epoch+1}: Train Loss: {train_loss:.6f}, Val Loss: {val_loss:.6f}')
 
-    
-# =============================================================================
-#     # Early stopping logic
-#     if val_loss < best_val_loss:
-#         best_val_loss = val_loss
-#         epochs_no_improvement = 0
-#     else:
-#         epochs_no_improvement += 1
-# 
-#     if epochs_no_improvement >= patience:
-#         print('Early stopping triggered.')
-#         break
-# =============================================================================
-
 print("Finished Training")
 
 def plot_graph(history):
@@ -244,5 +214,154 @@ def plot_graph(history):
 plot_graph(training_log)
 
 # Save Deep Learning Model
-save_path = "ABC_New_Tune_BS32_2.pth"
-torch.save(model.state_dict(), save_path)
+#save_path = "ABC_D2_H1000_N2_BS32_final.pth"
+#torch.save(model.state_dict(), save_path)
+
+#%%
+model.to('cpu')
+df_name_smile = df[['Name','SMILES']]
+df_T = (df["Tmin"]+df["Tmax"])/2
+train_T = pd.concat([df_name_smile, Y_train, df_T], axis=1, join="inner")
+test_T = pd.concat([df_name_smile, Y_test ,df_T], axis=1, join="inner")
+
+pred0 = model(torch.DoubleTensor(x_train.values)) #Train
+pred1 = model(torch.DoubleTensor(x_test.values)) #Test
+inv_pred0 = scaler.inverse_transform(pred0.detach().numpy())
+inv_pred1 = scaler.inverse_transform(pred1.detach().numpy())
+
+#Train
+df_com0 = pd.DataFrame({
+    "Name": train_T["Name"],
+    "SMILES": train_T["SMILES"],
+    "A_act" : Y_train["A"],
+    "A_pred": inv_pred0[:,0],
+    "B_act" : Y_train["B"],
+    "B_pred": inv_pred0[:,1],
+    "C_act" : Y_train["C"],
+    "C_pred": inv_pred0[:,2],
+    "T": train_T.iloc[:,-1],
+})
+
+df_com0["lnPsat_Act"] = df_com0["A_act"]-(df_com0["B_act"]/(df_com0["T"]+df_com0["C_act"]))
+df_com0["lnPsat_Cal"] = df_com0["A_pred"]-(df_com0["B_pred"]/(df_com0["T"]+df_com0["C_pred"]))
+
+#Test
+df_com1 = pd.DataFrame({
+    "Name": test_T["Name"],
+    "SMILES": test_T["SMILES"],
+    "A_act" : Y_test["A"],
+    "A_pred": inv_pred1[:,0],
+    "B_act" : Y_test["B"],
+    "B_pred": inv_pred1[:,1],
+    "C_act" : Y_test["C"],
+    "C_pred": inv_pred1[:,2],
+    "T": test_T.iloc[:,-1],
+})
+
+df_com1["lnPsat_Act"] = df_com1["A_act"]-(df_com1["B_act"]/(df_com1["T"]+df_com1["C_act"]))
+df_com1["lnPsat_Cal"] = df_com1["A_pred"]-(df_com1["B_pred"]/(df_com1["T"]+df_com1["C_pred"]))
+
+#%% Metrics Test
+mae_test = mean_absolute_error(df_com1["A_act"], df_com1["A_pred"]), mean_absolute_error(df_com1["B_act"], df_com1["B_pred"]), mean_absolute_error(df_com1["C_act"], df_com1["C_pred"]), mean_absolute_error(df_com1["lnPsat_Act"], df_com1["lnPsat_Cal"])
+mape_test = mean_absolute_percentage_error(df_com1["A_act"], df_com1["A_pred"]), mean_absolute_percentage_error(df_com1["B_act"], df_com1["B_pred"]), mean_absolute_percentage_error(df_com1["C_act"], df_com1["C_pred"]), mean_absolute_percentage_error(df_com1["lnPsat_Act"], df_com1["lnPsat_Cal"])
+mse_test = mean_squared_error(df_com1["A_act"], df_com1["A_pred"]), mean_squared_error(df_com1["B_act"], df_com1["B_pred"]), mean_squared_error(df_com1["C_act"], df_com1["C_pred"]), mean_squared_error(df_com1["lnPsat_Act"], df_com1["lnPsat_Cal"])
+R2_test = r2_score(df_com1["A_act"], df_com1["A_pred"]),r2_score(df_com1["B_act"], df_com1["B_pred"]),r2_score(df_com1["C_act"], df_com1["C_pred"]), r2_score(df_com1["lnPsat_Act"], df_com1["lnPsat_Cal"])
+
+print("MAE","\nA = ",mae_test[0],"\nB = ",mae_test[1],"\nC = ",mae_test[2],"\nLogVP = ", mae_test[3])
+print("MAPE","\nA = ",mape_test[0],"\nB = ",mape_test[1],"\nC = ",mape_test[2],"\nLogVP = ", mape_test[3])
+print("RMSE","\nA = ",np.sqrt(mse_test[0]),"\nB = ",np.sqrt(mse_test[1]),"\nC = ",np.sqrt(mse_test[2]),"\nLogVP = ", np.sqrt(mse_test[3]))
+print("R2","\nA = ",R2_test[0],"\nB = ",R2_test[1],"\nC = ",R2_test[2],"\nLogVP = ", R2_test[3])
+
+#%% Visualization
+
+x_min = min(min(df_com1["A_pred"]),min(df_com1["A_act"]))
+x_max = max(max(df_com1["A_pred"]),max(df_com1["A_act"]))
+y_min, y_max = x_min, x_max
+
+x = np.linspace(x_min, x_max, 100)
+y = x
+
+# PyPlot
+plt.plot([x_min, x_max], [y_min, y_max], color="black", alpha=0.5, linestyle="--")
+plt.scatter(df_com1["A_act"], df_com1["A_pred"], alpha=0.5, color='black')
+plt.xlabel("Actual")
+plt.ylabel("Predictions")
+plt.title("A")
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+
+#%%
+x_min = min(min(df_com1["B_pred"]),min(df_com1["B_act"]))
+x_max = max(max(df_com1["B_pred"]),max(df_com1["B_act"]))
+y_min, y_max = x_min, x_max
+
+x = np.linspace(x_min, x_max, 100)
+y = x
+
+# PyPlot
+plt.plot([x_min, x_max], [y_min, y_max], color="black", alpha=0.5, linestyle="--")
+plt.scatter(df_com1["B_act"], df_com1["B_pred"], alpha=0.5, color='black')
+plt.xlabel("Actual")
+plt.ylabel("Predictions")
+plt.title("B")
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+
+#%%
+x_min = min(min(df_com1["C_pred"]),min(df_com1["C_act"]))
+x_max = max(max(df_com1["C_pred"]),max(df_com1["C_act"]))
+y_min, y_max = x_min, x_max
+
+x = np.linspace(x_min, x_max, 100)
+y = x
+
+# PyPlot
+plt.plot([x_min, x_max], [y_min, y_max], color="black", alpha=0.5, linestyle="--")
+plt.scatter(df_com1["C_act"], df_com1["C_pred"], alpha=0.5, color='black')
+plt.xlabel("Actual")
+plt.ylabel("Predictions")
+plt.title("C")
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+
+#%%
+x_min = min(min(df_com1["lnPsat_Cal"]),min(df_com1["lnPsat_Act"]))
+x_max = max(max(df_com1["lnPsat_Cal"]),max(df_com1["lnPsat_Act"]))
+y_min, y_max = x_min, x_max
+
+x = np.linspace(x_min, x_max, 100)
+y = x
+
+# PyPlot
+plt.plot([x_min, x_max], [y_min, y_max], color="black", alpha=0.5, linestyle="--")
+plt.scatter(df_com1["lnPsat_Act"], df_com1["lnPsat_Cal"], alpha=0.5, color='black')
+plt.xlabel("Actual")
+plt.ylabel("Calculation")
+plt.title("lnVP")
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+
+#%%
+df_com1["Vapor_Pressure_Act"] = np.exp(df_com1['lnPsat_Act'])/10**5
+df_com1["Vapor_Pressure_Cal"] = np.exp(df_com1['lnPsat_Cal'])/10**5
+
+#%%
+x_min = min(min(df_com1["Vapor_Pressure_Cal"]),min(df_com1["Vapor_Pressure_Act"]))
+x_max = max(max(df_com1["Vapor_Pressure_Cal"]),max(df_com1["Vapor_Pressure_Act"]))
+y_min, y_max = x_min, x_max
+
+x = np.linspace(x_min, x_max, 100)
+y = x
+
+# PyPlot
+plt.plot([x_min, x_max], [y_min, y_max], color="black", alpha=0.5, linestyle="--")
+plt.scatter(df_com1["Vapor_Pressure_Act"], df_com1["Vapor_Pressure_Cal"], alpha=0.5, color='black')
+plt.xlabel("Vapor Pressure (atm) Actual")
+plt.ylabel("Vapor Pressure (atm) Cal")
+plt.title("Vapor Pressure (atm)")
+plt.xlim(x_min, x_max)
+plt.ylim(y_min, y_max)
+
+#%%
+df_com0.to_csv("Train_Act_Pred_Cal.csv")
+df_com1.to_csv("Test_Act_Pred_Cal.csv")
